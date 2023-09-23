02bd6b014f3f7be5c544bdac95032ddd711dda59
TejaManchikatla <cs21b031@iittp.ac.in>
2023-09-23 19:40:30 +0530

    added a bar that shows video progress, we have a bug will explain below

--- client/src/web/components/short/shortContent.jsx
+++ client/src/web/components/short/shortContent.jsx
@@ -3,14 +3,35 @@ import playButton from "../../../../src/images/play.svg"
 import pauseButton from "../../../../src/images/pause.svg"
 
 const ShortContent = ({videoRef}) => {
-    const [toDisplay, setToDisplay]=useState(playButton);
-   
-
+    const [currentTime, setCurrentTime] = useState(0);
+    let duration;
     
+    const getDurationOfVideo = () => {
 
+        const videoIntervalTime = setInterval(() => {
+
+            setCurrentTime(parseFloat(videoRef.current.currentTime));
+
+            if (parseFloat(videoRef.current.currentTime) >= duration)
+            {
+
+                clearVideoInterval();
+            }
+
+        }, 1000)
+
+
+
+        const clearVideoInterval = () => {
+            clearInterval(videoIntervalTime);
+        }
+
+    }
 
     const videoPlay = () => {
+        duration = videoRef.current.duration;
         videoRef.current.play();
+        getDurationOfVideo();
     }
     const videoPause = ()=>{
         videoRef.current.pause();
@@ -20,11 +41,9 @@ const ShortContent = ({videoRef}) => {
         
         
         if(videoRef.current.paused){
-            setToDisplay(playButton);
             videoPlay();
         }
         else{
-            setToDisplay(pauseButton);
             videoPause();
         }
 
@@ -33,11 +52,12 @@ const ShortContent = ({videoRef}) => {
     return ( 
         <div className="shortContent" onClick={()=>{handlePausePlay()}}>
             
-            <video ref={videoRef} width="100%" height="100%" autoPlay loop>
+            <video ref={videoRef} width="100%" height="100%" autoPlay loop style={{objectFit:"fill", borderRadius:"1rem"}}>
                 <source src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg" />
             </video>
+            <div id = "videoDurationBar" style={{width:""+((parseFloat(videoRef.current.currentTime).toFixed(6)/parseFloat(videoRef.current.duration))*93.5)+"%", height:"0.2rem", backgroundColor:"red",borderRadius:"0.25rem", translate:"0.5rem -0.6rem 0rem"}}></div>
+            
         </div>
      );
 }
- 
 export default ShortContent;
