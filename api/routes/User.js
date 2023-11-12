0d07d4bb717f86fd4a5d2daadecee34d65137698
Sudharshan S <93808025+Sudharshan281@users.noreply.github.com>
2023-11-12 22:53:37 +0530

    Added getAllUsers

--- api/routes/User.js
+++ api/routes/User.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getUser, updateUser, deleteUser, followUser, unFollowUser } = require("../controllers/UserController");
+const { getUser, updateUser, deleteUser, followUser, unFollowUser, getAllUsers } = require("../controllers/UserController");
 
 const router = express.Router();
 
+router.get('/', getAllUsers);
 router.get('/:id', getUser)
 router.put('/:id', updateUser)  
 router.put('/:id/follow', followUser)  
