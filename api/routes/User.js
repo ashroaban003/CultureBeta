d0edb1e21fae2c2f6dbf03aff7cafcb45fb81971
Sudharshan S <93808025+Sudharshan281@users.noreply.github.com>
2023-11-13 14:14:18 +0530

    Update User.js

--- api/routes/User.js
+++ api/routes/User.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getUser, updateUser, deleteUser, followUser, unFollowUser, getAllUsers } = require("../controllers/UserController");
+const { getUser, updateUser, deleteUser, followUser, unFollowUser, getAllUsers, isUserFollowing } = require("../controllers/UserController");
 
 const router = express.Router();
 
@@ -9,4 +9,6 @@ router.put('/:id', updateUser)
 router.put('/:id/follow', followUser)  
 router.put('/:id/unfollow', unFollowUser)  
 router.delete('/:id', deleteUser)
+router.get('/:id/follow/:id2', isUserFollowing)
+
 module.exports = router;
