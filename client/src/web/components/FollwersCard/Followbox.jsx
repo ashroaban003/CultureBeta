631072cdbb79e554a6e1ff0e3fe25c5055b1dcfe
TejaManchikatla <cs21b031@iittp.ac.in>
2023-11-13 15:28:05 +0530

    necessaryuichngs,followunfollowbugfixed

--- client/src/web/components/FollwersCard/Followbox.jsx
+++ client/src/web/components/FollwersCard/Followbox.jsx
@@ -2,58 +2,106 @@ import { useContext, useState } from "react"
 import { AuthContext } from "../../context/AuthContext"
 import axios from "axios";
 import useFetch1 from "../../hooks/usefetch1";
+import { useEffect } from "react";
 
 export default function Followbox({follower,reFetch}) {
     const {user}=useContext(AuthContext);
     const [flw,setflw]=useState({
         curUserId : user.id,
     });
+    
     const [suc,setsuc]=useState("false");
+    
     const {data1}=useFetch1(`http://localhost:4000/api/user/${user.id}/follow/${follower._id}`)
-    console.log(data1);
+    //console.log("THE DATA", data1);
+    const [followText, setFollowText] = useState("");
+    
+    // const handleclick =async (e) => {
+    //     e.preventDefault();
+    //     try{
+    //     const res=await axios.put(`http://localhost:4000/api/user/${follower._id}/follow/`,flw);
+    //     if(res){
+    //         setsuc("success");
+    //         reFetch();
+    //         setFollowText("UnFollow");
+    //     } 
+    //     }catch(e){
+    //         setsuc("failee");
+    //         console.error(e.response || e);
+    //     }
+    // }
+    // const handleunfollow = async (e) => {
+    //     e.preventDefault();
+    //     try{
+    //     const res=await axios.put(`http://localhost:4000/api/user/${follower._id}/unfollow/`,flw);
+    //     if(res){
+    //         setsuc("success");
+    //         reFetch();
+    //         setFollowText("Follow");
+    //     } 
+    //     }catch(e){
+    //         setsuc("failee");
+    //         console.error(e.response || e);
+    //     }
+    // }
+
+
     const handleclick =async (e) => {
         e.preventDefault();
-        try{
-        const res=await axios.put(`http://localhost:4000/api/user/${follower._id}/follow/`,flw);
-        if(res){
-            setsuc("success");
-            reFetch();
-        } 
-        }catch(e){
-            setsuc("failee");
-            console.error(e.response || e);
+        if(followText=="Follow"){
+            try{
+                const res=await axios.put(`http://localhost:4000/api/user/${follower._id}/follow/`,flw);
+                if(res){
+                    setsuc("success");
+                    reFetch();
+                    setFollowText("UnFollow");
+                } 
+            }catch(e){
+                setsuc("failee");
+                console.error(e.response || e);
+                }
+            }
+        else{
+            try{
+                const res=await axios.put(`http://localhost:4000/api/user/${follower._id}/unfollow/`,flw);
+                if(res){
+                    setsuc("success");
+                    reFetch();
+                    setFollowText("Follow");
+                } 
+                }catch(e){
+                    setsuc("failee");
+                    console.error(e.response || e);
+                }
         }
     }
-    const handleunfollow = async (e) => {
-        e.preventDefault();
-        try{
-        const res=await axios.put(`http://localhost:4000/api/user/${follower._id}/unfollow/`,flw);
-        if(res){
-            setsuc("success");
-            reFetch();
-        } 
-        }catch(e){
-            setsuc("failee");
-            console.error(e.response || e);
-        }
-    }
+
+    useEffect(()=>{
+        //console.log("inside USEFFECT", data1, data1.isfollow);
+        setFollowText(data1.isfollow ? "UnFollow":"Follow");
+    }, [data1]);
+
+
     return(
         <div className="follower">
                     <div>
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzEkveEWaWSZ6ytqtnxs7r3ObfsL07gjHsZg&usqp=CAU" alt="" className='followerImage' />
                         <div className="name">
-                            <span>{follower.username}</span>
+                            <span style={{width: "10rem", overflow:"hidden", textOverflow:"ellipsis"}}>{follower.username}</span>
                             <span> {(follower.followers).length} Followers</span>
                         </div>
                     </div>
                     {/* <span>{suc}</span> */}
-                  {!data1.isfollow &&  <button className="followButton" onClick={handleclick} style={{backgroundColor : "linear-gradient(to right, #0074e4, #00a1ff);"}}>
-                        Follow
+                  {/* {!data1.isfollow &&  <button className="followButton" onClick={handleclick} style={{backgroundColor : "linear-gradient(to right, #0074e4, #00a1ff);"}}>
+                        {followText}
                     </button>}
                    {data1.isfollow &&
                     <button className="followButton" onClick={handleunfollow} style={{backgroundColor : "linear-gradient(to right, #0074e4, #00a1ff);"}}>
-                        UnFollow
-                    </button>} 
+                        {followText}
+                    </button>} */}
+                    <button className="followButton" onClick={handleclick} style={{backgroundColor : "linear-gradient(to right, #0074e4, #00a1ff);"}}>
+                        {followText}
+                    </button>
          </div>
     )
 };
