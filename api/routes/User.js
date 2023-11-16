b09aab93c325210650e55c913e54e0c2355f6efc
Sudharshan S <93808025+Sudharshan281@users.noreply.github.com>
2023-11-16 23:05:36 +0530

    Update User.js to get profile page route

--- api/routes/User.js
+++ api/routes/User.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { getUser, updateUser, deleteUser, followUser, unFollowUser, getAllUsers, isUserFollowing } = require("../controllers/UserController");
+const { getUser, updateUser, deleteUser, followUser, unFollowUser, getAllUsers, isUserFollowing, getUserInfo } = require("../controllers/UserController");
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
 router.get('/:id', getUser)
 router.put('/:id', updateUser)  
+router.get('/:id/profile', getUserInfo);
 router.put('/:id/follow', followUser)  
 router.put('/:id/unfollow', unFollowUser)  
 router.delete('/:id', deleteUser)
